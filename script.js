5c45842d3fc3e35a5bb13bbd378a15420d1d2bd0
Paul Fresnel <paul.fresnel1@gmail.com>
2022-10-03 19:13:42 +0200

    functional calculator, 1rst version

---
+++ script.js
@@ -0,0 +1,55 @@
+function addNumber(event){
+  const target = event.currentTarget;
+    const number = parseInt(event.currentTarget.innerHTML);
+    console.log(number);
+    console.log('working?');
+  updateField(number);
+}
+
+function updateField(input){
+  document.getElementById('visualization').firstChild.textContent += input;
+}
+
+function addition(){
+  document.getElementById('visualization').firstChild.textContent += '+';
+}
+
+function substraction(){
+    document.getElementById('visualization').firstChild.textContent += '-';
+}
+
+function multiplication(){
+    document.getElementById('visualization').firstChild.textContent += '*';
+}
+
+function division(){
+    document.getElementById('visualization').firstChild.textContent += '/';
+}
+
+function calculate() {
+  
+  let result = document.getElementById('visualization').textContent
+
+  document.getElementById('visualization').firstChild.textContent = eval(result);
+}
+
+function reset(){
+  document.getElementById('visualization').innerHTML = '<p></p>';
+}
+
+Window.addEventListener('load',() => {
+  document.querySelectorAll('.cell').forEach((button) => {
+button.addEventListener('click', addNumber)
+});
+  document.querySelector('#addition').addEventListener('click',addition);
+  document.querySelector('#substraction').addEventListener('click',substraction);
+  document.querySelector('#multiplication').addEventListener('click',multiplication);
+  
+  document.querySelector('#division').addEventListener('click',division);
+  document.querySelector('#calculate').addEventListener('click',calculate);
+  
+    document.querySelector('#reset').addEventListener('click',reset);
+
+
+    console.log("fully loaded");
+});
